a3987027d0b469792cc18fb67888e7ab3c3a8a4c
Matheus <matrodrigues@live.com>
2022-04-09 17:15:11 -0300

    Add game control function

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let userChoice;
 let userChoiceNumber;
+let userWins;
 let computerValue;
 let computerValueNumber;
+let computerWins;
+let draws;
+
 
 
 function getUserChoice(){
-    userChoice = prompt("Do you want to choose rock, paper or scissor?");
+    userChoice = prompt("Do you want to choose rock, paper or scissor?").toLowerCase();
     while(userChoice != "rock" && userChoice != "paper" && userChoice != "scissor"){
-        userChoice = prompt("Please enter only rock, paper or scissor. Which one do you want?");
+        userChoice = prompt("Please enter only rock, paper or scissor. Which one do you want?").toLowerCase();
     }
-    
     return userChoice;
 }
 
@@ -22,15 +25,35 @@ function selectComputerValue(){
     return computerValue; 
 }
 
-function getRoundWinner(){
+function getRoundWinner(getUserChoice, selectComputerValue){
     getUserChoice();
     selectComputerValue();
     if(userChoice == "rock" && computerValue == "scissor" || userChoice == "scissor" && computerValue == "paper" || userChoice == "paper" && computerValue == "rock"){
-        return "Player won!";
+        userWins++;
+        console.log(userWins);
+        console.log("Player won!");
     } else if (userChoice == computerValue){
-        return "It's a draw!";
+        draws++;
+        console.log("It's a draw!");
     } else {
-        return "Computer won!";
+        computerWins++;
+        console.log("Computer won!");
     }
 }
 
+function game(){
+    let rounds = 0;
+    userWins = 0;
+    computerWins = 0;
+    draws = 0; 
+    while (rounds < 5){
+        getRoundWinner(getUserChoice, selectComputerValue);
+        rounds++;
+    }
+    if (userWins > computerWins){
+        return "Player won the game!";
+     } else if (userWins < computerWins) {
+         return "Computer won the game!";
+    }
+    return "The game ended in a draw!";
+}
