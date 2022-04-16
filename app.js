1cb2b5075b739ab85ba94e46dfd3d0a921ebc5d1
Matheus <matrodrigues@live.com>
2022-04-16 19:52:08 -0300

    Refactor game logic and end styling

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ let computerValue;
 let computerValueNumber;
 let computerWins = 0;
 let draws;
+let userCapitalizedValue;
+let computerCapitalizedValue;
+let winningWord;
+let computerWinningWord;
 
 
 const para = document.getElementById('resultText');
@@ -63,16 +67,14 @@ function selectComputerValue(){
 
 function getRoundWinner(userValue, selectComputerValue){
     userValue = userChoice;
+    userCapitalizedValue = userValue[0].toUpperCase() + userValue.substring(1);
     selectComputerValue();
-     if (userValue == "rock" && computerValue == "scissor" || userValue == "scissor" && computerValue == "paper" 
-     || userValue == "paper" && computerValue == "rock" || userValue == "rock" && computerValue == "lizard" 
-     || userValue == "scissor" && computerValue == "lizard" || userValue == "paper" && computerValue == "spock" 
-     || userValue == "lizard" && computerValue == "paper" || userValue == "lizard" && computerValue == "spock" 
-     || userValue == "spock" && computerValue == "rock" || userValue == "spock" && computerValue == "scissor"){
+    computerCapitalizedValue = computerValue[0].toUpperCase() + computerValue.substring(1);
+     if (handleGameLogic(userValue, computerValue)){
             userWins++;
-            para.textContent = "Player Won!";
+            para.textContent = `Player won! ${userCapitalizedValue} ${handlePlayerWinningWord(userValue,computerValue)} ${computerCapitalizedValue}!`;
             paraUserWins.textContent = `${userWins}`;
-            console.log(`Player won!${userValue} beats ${computerValue}!`);
+            console.log(`Player won! ${userCapitalizedValue} beats ${computerCapitalizedValue}!`);
             if(userWins == 5){
                 para.textContent = "Player won the game!";
                 disableButtons();   
@@ -84,12 +86,67 @@ function getRoundWinner(userValue, selectComputerValue){
             return draws;
         } else {
             computerWins++;
-            para.textContent = "Computer won!";
+            para.textContent = `Computer won! ${computerCapitalizedValue} ${handleComputerWinningWord(userValue, computerValue)} ${userCapitalizedValue}!`;
             paraComputerWins.textContent = `${computerWins}`;
-            console.log(`Computer won!${computerValue} beats ${userValue}!`);
+            console.log(`Computer won!${computerValue} beats ${userCapitalizedValue}!`);
             if (computerWins == 5){
                 para.textContent = "Computer won the game!";
                 disableButtons();  
             }
         }
     }  
+
+function handleGameLogic(userValue, computerValue){
+    if (userValue == "rock" && computerValue == "scissor" || userValue == "scissor" && computerValue == "paper" 
+     || userValue == "paper" && computerValue == "rock" || userValue == "rock" && computerValue == "lizard" 
+     || userValue == "scissor" && computerValue == "lizard" || userValue == "paper" && computerValue == "spock" 
+     || userValue == "lizard" && computerValue == "paper" || userValue == "lizard" && computerValue == "spock" 
+     || userValue == "spock" && computerValue == "rock" || userValue == "spock" && computerValue == "scissor"){
+         return true;
+     }
+}    
+
+function handlePlayerWinningWord(userValue, computerValue) {
+    if (userValue == "rock" && computerValue == "scissor" || userValue == "rock" && computerValue == "lizard") {
+        winningWord = "crushes";
+    } else if (userValue == "scissor" && computerValue == "paper") {
+        winningWord == "cuts";
+    } else if (userValue == "paper" && computerValue == "rock") {
+        winningWord = "covers";
+    } else if (userValue == "scissor" && computerValue == "lizard") {
+        winningWord = "decapitates";
+    } else if (userValue == "paper" && computerValue == "spock") {
+        winningWord = "disproves";
+    } else if (userValue == "lizard" && computerValue == "paper") {
+        winningWord = "eats";
+    } else if (userValue == "lizard" && computerValue == "spock") {
+        winningWord = "poisons";
+    } else if (userValue == "spock" && computerValue == "rock") {
+        winningWord = "vaporizes";
+    } else if (userValue == "spock" && computerValue == "scissor") {
+        winningWord = "smashes";
+    } return winningWord;
+}
+     
+function handleComputerWinningWord(userValue, computerValue) {
+    if (computerValue == "rock" && userValue == "scissor" || computerValue == "rock" && userValue == "lizard") {
+        computerWinningWord = "crushes";
+    } else if (computerValue == "scissor" && userValue == "paper") {
+        computerWinningWord == "cuts";
+    } else if (computerValue == "paper" && userValue == "rock") {
+        computerWinningWord = "covers";
+    } else if (computerValue == "scissor" && userValue == "lizard") {
+        computerWinningWord = "decapitates";
+    } else if (computerValue == "paper" && userValue == "spock") {
+        computerWinningWord = "disproves";
+    } else if (computerValue == "lizard" && userValue == "paper") {
+        computerWinningWord = "eats";
+    } else if (computerValue == "lizard" && userValue == "spock") {
+        computerWinningWord = "poisons";
+    } else if (computerValue == "spock" && userValue == "rock") {
+        computerWinningWord = "vaporizes";
+    } else if (computerValue == "spock" && userValue == "scissor") {
+        computerWinningWord = "smashes";
+    } return computerWinningWord;
+}   
+    
